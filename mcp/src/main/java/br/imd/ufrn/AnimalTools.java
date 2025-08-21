42309a03efe6a1ef1a23cec81c4bac17f3365004
CinquilCinquil <carlosvallerosafilho@gmail.com>
2025-08-21 20:35:39 -0300

    feat: Add mcp server code

---
+++ mcp/src/main/java/br/imd/ufrn/AnimalTools.java
@@ -0,0 +1,23 @@
+package br.imd.ufrn;
+
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnimalTools {
+
+    @Tool(description = "Returns true if the animal is a fish, false otherwise." +
+     "Must be used when asked to verify if a certain species if a fish species.")
+    String isFish(@ToolParam(description = "Animal species") String species) {
+        System.out.print("Verifying if " + species + " is a fish");
+        return String.valueOf(!species.equals("Not Fishius"));
+    }
+
+    @Tool(name= "Animal Register",
+     description = "Registers an animal. Must be used when asked to register an animal.")
+    String registerAnimal(@ToolParam(description="Animal name to be registered") String name) {
+        System.out.print("Hurray " + name + "!!!!!");
+        return "Animal " + name + "has been registered!";
+    }
+}
