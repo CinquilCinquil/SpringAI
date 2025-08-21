73d0d68327fcff795a91df2c30645afc60545620
CinquilCinquil <carlosvallerosafilho@gmail.com>
2025-08-21 20:33:10 -0300

    feat: Add mcp

--- src/main/java/com/imd/ufrn/prompt/Service/AnimalsServiceImpl.java
+++ src/main/java/com/imd/ufrn/prompt/Service/AnimalsServiceImpl.java
@@ -2,9 +2,21 @@ package com.imd.ufrn.prompt.Service;
 
 import java.util.List;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.ChatMemoryRepository;
+import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.document.Document;
+import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
+
 import com.imd.ufrn.prompt.Utils.DocumentReader;
+
+import io.modelcontextprotocol.client.McpSyncClient;
+
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.imd.ufrn.prompt.DAO.AnimalsDAO;
@@ -15,9 +27,24 @@ public class AnimalsServiceImpl implements AnimalsService {
     private final AnimalsDAO animalsDao;
     private final DocumentReader documentReader;
 
-    public AnimalsServiceImpl(AnimalsDAO animalsDao, DocumentReader documentReader) {
+    @Autowired
+    VectorStore vectorStore;
+
+    ChatMemoryRepository chatMemoryRepository = new InMemoryChatMemoryRepository();
+    ChatMemory chatMemory = MessageWindowChatMemory.builder()
+                .chatMemoryRepository(chatMemoryRepository)
+                .maxMessages(10)
+                .build();
+
+    private final ChatClient chatClient;
+
+    public AnimalsServiceImpl(AnimalsDAO animalsDao, DocumentReader documentReader,
+        ChatClient.Builder chatClientBuilderOpenAI, List<McpSyncClient> mcpClients) {
         this.animalsDao = animalsDao;
         this.documentReader = documentReader;
+        this.chatClient = chatClientBuilderOpenAI
+            .defaultToolCallbacks(new SyncMcpToolCallbackProvider(mcpClients))
+            .build();
     }
 
     @Override
